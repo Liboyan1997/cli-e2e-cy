b2f7a3edb501930d0dbccd1ef0839928736f142e
heibai <lijing@shangyoo.com>
2023-08-03 14:11:20 +0800

    fix: 文件排版

--- generator/index.js
+++ generator/index.js
@@ -1,11 +1,12 @@
-module.exports = api => { 
+module.exports = api => {
   api.render('./template')
-    api.extendPackage({
-      devDependencies: {
-        cypress: require('../package.json').devDependencies.cypress
-      },
-      scripts: {
-        'test:e2e': 'vue-cli-service test:e2e'
-      }
-    })
-  }
+
+  api.extendPackage({
+    devDependencies: {
+      cypress: require('../package.json').devDependencies.cypress
+    },
+    scripts: {
+      'test:e2e': 'vue-cli-service test:e2e'
+    }
+  })
+}
